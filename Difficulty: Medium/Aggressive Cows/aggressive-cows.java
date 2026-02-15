13b2ef6926638a743ea57e1bf0ff5dd3faeac7f1
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-02-15 21:04:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,50 +1,54 @@
 class Solution {
     public int aggressiveCows(int[] stalls, int k) {
-       
-       int n = stalls.length;
-       Arrays.sort(stalls);
-       int start = 1;
-       int dist = 0;
-       int min = Integer.MAX_VALUE;
-       int max = Integer.MIN_VALUE;
-       
-       for(int num : stalls) {
-           min = Math.min(num, min);
-           max = Math.max(num, max);
-       }
-       
-       dist = max - min;
-       int end = dist;
-       int ans = start;
-       
-       while(start <= end) {
-           
-           int mid = (start + end) /2;
-           int possibleNoOfCows = getNoOfCows(stalls, mid);
-           if (possibleNoOfCows >= k) {
-               ans = mid;
-               start = mid + 1;
-           }
-           else {
-               end = mid - 1;
-           }
-       }
-       
-       return ans;
         
-    }
-    
-    public int getNoOfCows(int[] stalls, int mid) {
+        Arrays.sort(stalls);
+        int n = stalls.length;
+        int start = 1;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         
-        int cows = 1;
-        int cowPlaced = 0;
-        for(int i = 1; i < stalls.length; i++) {
-            if((stalls[i] - stalls[cowPlaced]) >= mid) {
-                cowPlaced = i;
-                cows += 1;
+        for(int stall : stalls) {
+            max = Math.max(stall, max);
+            min = Math.min(stall, min);
+        }
+        
+        int end = max - min;
+        int ans = 1;
+        
+        while(start <= end) {
+            
+            int mid = (start + end)/2;  
+            int possibleCows = getPossibleCowsNo(stalls, mid);
+            
+            if(possibleCows >= k) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
             }
         }
         
+        return ans;
+        
+    }
+    
+    public int getPossibleCowsNo(int[] stalls, int mid) {
+        
+        int i = 0; 
+        int j = 1;
+        int cows = 1;
+        int n = stalls.length;
+        
+        while(j < n) {
+            if((stalls[j] - stalls[i] )>= mid) {
+                cows += 1;
+                i = j;
+            }
+            j++;
+        }
+        
         return cows;
+        
     }
 }
