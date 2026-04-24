4739cb72c365463ebe53f04089e968fd5917cc85
Mansi <135570771+MansiNaxine@users.noreply.github.com>
2026-04-24 14:30:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,72 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+class Solution {
+    public ArrayList<Integer> topView(Node root) {
+        
+        Queue<Pair> que = new LinkedList<>();
+        Pair p =new Pair();
+        p.node = root;
+        p.cnt = 0;
+        que.add(p);
+        Map<Integer, Integer> map = new TreeMap<>();
+        
+        while(!que.isEmpty()) {
+            int size = que.size();
+            
+            for(int i = 0; i < size; i++) {
+                Pair pNew = que.peek();
+                Node newNode = pNew.node;
+                int count = pNew.cnt;
+                que.remove();
+                
+                if(!map.containsKey(count)) {
+                    map.put(count, newNode.data);
+                }
+                
+                if(newNode.left != null) {
+                    Pair innerP = new Pair();
+                    innerP.node = newNode.left;
+                    innerP.cnt = count - 1;
+                    que.add(innerP);
+                }
+                
+                if(newNode.right != null) {
+                    Pair innerP = new Pair();
+                    innerP.node = newNode.right;
+                    innerP.cnt = count + 1;
+                    que.add(innerP);
+                }
+                
+            }
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            result.add(entry.getValue());
+        }
+        
+        return result;
+        
+    }
+}
+
+class Pair {
+    Node node;
+    int cnt;
+
+    Pair() {
+        this.node = null;
+        cnt = 0;
+    }
+
+}
